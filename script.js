8dd6eabbad5beced3c7ccd55499121cabcff5b99
Abdul Raheem <61801382+abdulraheem286@users.noreply.github.com>
2021-05-05 18:52:58 +0500

    validation

--- script.js
+++ script.js
@@ -9,13 +9,27 @@ window.onload = function () {
 
 function handleAdd() {
   var table = document.getElementById("table");
-  var ageval = document.getElementById("age").value;
 
-  if (ageval >= 18 && ageval <= 65) {
-    alert("age not correct");
+  // Validations;
+  var val = document.getElementById("name").value;
+  if (!val.match(/^[a-zA-Z]+$/)) {
+    alert("Only Alphabets Allowed");
+    return false;
+  }
+
+  var ageval = $("#age").val();
+  if (ageval <= 10 || ageval >= 50) {
+    alert("Age not correct");
     return;
   }
 
+  var selval = $("#city").val();
+  if (selval == "") {
+    alert("Select City");
+    return;
+  }
+
+  //Insert DATA
   var data = table.insertRow(-1);
   var name = data.insertCell(0);
   var gender = data.insertCell(1);
@@ -30,7 +44,7 @@ function handleAdd() {
 
   var btnupd = document.createElement("input");
   btnupd.type = "button";
-  btnupd.className = "btn btn-danger";
+  btnupd.className = "btn btn-danger btnupdate";
   btnupd.value = "update";
   btnupd.onclick = update_row;
   data.appendChild(btnupd);
@@ -38,11 +52,13 @@ function handleAdd() {
 
   var btndel = document.createElement("input");
   btndel.type = "button";
-  btndel.className = "btn btn-dark";
+  btndel.className = "btn btn-dark btndelete";
   btndel.value = "Delete";
   btndel.onclick = delete_row;
   data.appendChild(btndel);
   action.appendChild(btndel);
+
+  return false;
 }
 
 function delete_row() {
@@ -53,12 +69,33 @@ function delete_row() {
 }
 
 function reset_table() {
+  $('.form input[type="text"]').val("");
+  $('.form input[type="number"]').val("");
+  // $('.form input[type="radio"]').val("");
+  $(".form select").val("");
   console.log("reset");
-  $("#table td").remove();
+  return false;
+  // $("#table td").remove();
 }
 
 function update_row() {
   console.log("update row");
-  $("#update").removeClass("disabled");
-  $("#add").addClass("disabled");
+
+  $("#updateMain").removeClass("disabled");
+  $("#add").prop("disabled", true);
+
+  //code for updating row
+  // var rowId = event.target.parentNode.parentNode.id;
+  //this gives id of tr whose button was clicked
+  // var data = document.getElementById(rowId).querySelectorAll(".row-data");
+  /*returns array of all elements with 
+"row-data" class within the row with given id*/
+
+  // var name = data[0].innerHTML;
+  // var age = data[1].innerHTML;
+
+  // alert("Name: " + name + "\nAge: " + age);
+
+  $("#add").prop("disabled", false);
+  return false;
 }
